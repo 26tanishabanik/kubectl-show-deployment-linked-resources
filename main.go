a64a045fe6a23c08b95464a332167b1f6155d9da
Tanisha Banik <26tanishabanik@gmail.com>
2023-04-01 14:09:03 +0530

    added go routines

--- main.go
+++ main.go
@@ -6,13 +6,16 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sync"
 
-	"k8s.io/client-go/tools/clientcmd"
 	"github.com/tufin/asciitree"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/kubernetes"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	pflag "github.com/spf13/pflag"
 )
+
+var wg sync.WaitGroup
 
 func HomeDir() string {
 	if h := os.Getenv("HOME"); h != "" {
@@ -21,10 +24,10 @@ func HomeDir() string {
 	return ""
 }
 
-func getKubeConfig() *string{
+func getKubeConfig() *string {
 	var kubeConfig *string
 	if home := HomeDir(); home != "" {
-		kubeConfig = flag.String("kubeconfig", filepath.Join(home, ".kube","config"), "")
+		kubeConfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "")
 	} else {
 		kubeConfig = flag.String("kubeconfig", "", "")
 	}
@@ -38,38 +41,81 @@ func ClientSetup() (*kubernetes.Clientset, error) {
 		config, err := clientcmd.BuildConfigFromFlags("", *kubeConfig)
 		if err != nil {
 			fmt.Printf("Error in new client config: %s\n", err)
-			return nil,err
+			return nil, err
 		}
 		clientset := kubernetes.NewForConfigOrDie(config)
 		return clientset, nil
 	}
-	
+
 	config, err := clientcmd.BuildConfigFromFlags("", pflag.Lookup("kubeconfig").Value.String())
 	if err != nil {
 		fmt.Printf("Error in new client config: %s\n", err)
-		return nil,err
+		return nil, err
 	}
 	clientset := kubernetes.NewForConfigOrDie(config)
 	return clientset, nil
 
 }
 
-func GetConfigMaps(clientSet *kubernetes.Clientset, deploymentName, namespace string) ([]string, error) {
-	var configMapList []string
+func GetService(clientSet *kubernetes.Clientset, deploymentName, namespace string, ch1 chan string) {
+	deployment, err := clientSet.AppsV1().Deployments(namespace).Get(context.Background(), deploymentName, metav1.GetOptions{})
+	if err != nil {
+		fmt.Println("error in get deployment: ", err)
+		return
+	}
+
+	selector, err := metav1.LabelSelectorAsSelector(deployment.Spec.Selector)
+	if err != nil {
+		fmt.Println("error in getting label selector: ", err)
+		return
+	}
+	service, err := clientSet.CoreV1().Services(namespace).List(context.Background(), metav1.ListOptions{LabelSelector: selector.String()})
+	if err != nil {
+		fmt.Println("error in getting service name: ", err)
+		return
+	}
+	// fmt.Println("service name: ", service.Items[0].Name)
+	ch1 <- service.Items[0].Name
+	defer wg.Done()
+}
+
+func GetReplicaSet(clientSet *kubernetes.Clientset, deploymentName, namespace string, ch2 chan string) {
+	deployment, err := clientSet.AppsV1().Deployments(namespace).Get(context.Background(), deploymentName, metav1.GetOptions{})
+	if err != nil {
+		fmt.Println("error in getting deployment: ", err)
+		return
+	}
+
+	selector, err := metav1.LabelSelectorAsSelector(deployment.Spec.Selector)
+	if err != nil {
+		fmt.Println("error in getting label selector: ", err)
+		return
+	}
+	replicaSet, err := clientSet.AppsV1().ReplicaSets(namespace).List(context.Background(), metav1.ListOptions{LabelSelector: selector.String()})
+	if err != nil {
+		fmt.Println("error in getting replica set name: ", err)
+		return
+	}
+	// fmt.Println("replicaset name: ", replicaSet.Items[0].Name)
+	ch2 <- replicaSet.Items[0].Name
+	defer wg.Done()
+}
+
+func GetConfigMap(clientSet *kubernetes.Clientset, deploymentName, namespace string, ch chan string) {
 	deployment, _ := clientSet.AppsV1().Deployments(namespace).Get(context.Background(), deploymentName, metav1.GetOptions{})
-	
+
 	for _, ct := range deployment.Spec.Template.Spec.Containers {
 		if ct.Env != nil {
 			for _, en := range ct.Env {
 				if en.ValueFrom != nil {
-					configMapList = append(configMapList, en.ValueFrom.ConfigMapKeyRef.Name)
+					ch <- en.ValueFrom.ConfigMapKeyRef.Name
 				}
 			}
 		}
 		if ct.EnvFrom != nil {
 			for _, enf := range ct.EnvFrom {
 				if enf.ConfigMapRef != nil {
-					configMapList = append(configMapList, enf.ConfigMapRef.Name)
+					ch <- enf.ConfigMapRef.Name
 				}
 			}
 		}
@@ -77,29 +123,29 @@ func GetConfigMaps(clientSet *kubernetes.Clientset, deploymentName, namespace st
 	if deployment.Spec.Template.Spec.Volumes != nil {
 		for _, vol := range deployment.Spec.Template.Spec.Volumes {
 			if vol.ConfigMap != nil {
-				configMapList = append(configMapList, vol.ConfigMap.Name)
+				ch <- vol.ConfigMap.Name
 			}
 		}
 	}
-	return configMapList, nil
+	defer wg.Done()
+	ch <- "/done"
 }
 
-func GetSecrets(clientSet *kubernetes.Clientset, deploymentName, namespace string) ([]string, error) {
-	var secretList []string
+func GetSecret(clientSet *kubernetes.Clientset, deploymentName, namespace string, ch chan string) {
 	deployment, _ := clientSet.AppsV1().Deployments(namespace).Get(context.Background(), deploymentName, metav1.GetOptions{})
-	
+
 	for _, ct := range deployment.Spec.Template.Spec.Containers {
 		if ct.Env != nil {
 			for _, en := range ct.Env {
 				if en.ValueFrom != nil {
-					secretList = append(secretList, en.ValueFrom.SecretKeyRef.Name)
+					ch <- en.ValueFrom.SecretKeyRef.Name
 				}
 			}
 		}
 		if ct.EnvFrom != nil {
 			for _, enf := range ct.EnvFrom {
 				if enf.SecretRef != nil {
-					secretList = append(secretList, enf.SecretRef.Name)
+					ch <- enf.SecretRef.Name
 				}
 			}
 		}
@@ -107,83 +153,91 @@ func GetSecrets(clientSet *kubernetes.Clientset, deploymentName, namespace strin
 	if deployment.Spec.Template.Spec.Volumes != nil {
 		for _, vol := range deployment.Spec.Template.Spec.Volumes {
 			if vol.Secret != nil {
-				secretList= append(secretList, vol.Secret.SecretName)
+				ch <- vol.Secret.SecretName
 			}
 		}
 	}
-	return secretList, nil
+	ch <- "/done"
+	defer wg.Done()
 }
 
-func GetVolumes(clientSet *kubernetes.Clientset, deploymentName, namespace string) ([]string, error) {
-	var volumeList []string
+func GetVolume(clientSet *kubernetes.Clientset, deploymentName, namespace string, ch chan string) {
 	deployment, _ := clientSet.AppsV1().Deployments(namespace).Get(context.Background(), deploymentName, metav1.GetOptions{})
-	
-	
+
 	if deployment.Spec.Template.Spec.Volumes != nil {
 		for _, vol := range deployment.Spec.Template.Spec.Volumes {
-			if vol.Secret == nil && vol.ConfigMap == nil  {
-				if vol.Name != ""{
-					volumeList= append(volumeList, vol.Name)
-				}	
+			if vol.Secret == nil && vol.ConfigMap == nil {
+				if vol.Name != "" {
+					ch <- vol.Name
+				}
 			}
 		}
 	}
-	return volumeList, nil
-}
-
-func GetResourcesRelatedToDeployment(deploymentName, namespace string) (string, string, []string, []string, []string, error) {
-	clientSet, err :=  ClientSetup()
-	if err != nil {
-		fmt.Println("error in creating client set: ", err)
-		return "", "", nil, nil, nil, err
-	}
-
-	deployment, err := clientSet.AppsV1().Deployments("default").Get(context.Background(), deploymentName, metav1.GetOptions{})
-	if err != nil {
-		fmt.Println("error in get deployment: ", err)
-		return  "", "", nil, nil, nil, err
-	}
-	
-	selector, _ := metav1.LabelSelectorAsSelector(deployment.Spec.Selector)
-	service, err := clientSet.CoreV1().Services("default").List(context.Background(), metav1.ListOptions{LabelSelector: selector.String()})
-	if err != nil {
-		fmt.Println("error in getting service name: ", err)
-		return  "", "", nil, nil, nil, err
-	}
-	replicaSet, err := clientSet.AppsV1().ReplicaSets("default").List(context.Background(), metav1.ListOptions{LabelSelector: selector.String()})
-	if err != nil {
-		fmt.Println("error in get replica sets: ", err)
-		return  "", "", nil, nil, nil, err
-	}
-	configMapList, err := GetConfigMaps(clientSet, deployment.Name, namespace)
-	if err != nil {
-		fmt.Println("error in get config map list: ", err)
-		return  "", "", nil, nil, nil, err
-	}
-	secretList, err := GetSecrets(clientSet, deployment.Name, namespace)
-	if err != nil {
-		fmt.Println("error in get secret list: ", err)
-		return  "", "", nil, nil, nil, err
-	}
-	volumeList, err := GetVolumes(clientSet, deployment.Name, namespace)
-	if err != nil {
-		fmt.Println("error in get secret list: ", err)
-		return  "", "", nil, nil, nil, err
-	}
-	return service.Items[0].Name, replicaSet.Items[0].Name, configMapList, secretList, volumeList, nil
+	ch <- "/done"
+	defer wg.Done()
 }
 
 func main() {
-	var configMap, secret, volume string
+	var configMapList, volumeList, secretList []string
 	deploymentNamespace := flag.String("n", "default", "kubernetes namespace")
 	deploymentName := flag.String("d", "", "kubernetes deployment name")
 	flag.Parse()
-	service, replicaSet, configMapList, secretList, volumeList, err := GetResourcesRelatedToDeployment(*deploymentName, *deploymentNamespace)
+	clientSet, err := ClientSetup()
 	if err != nil {
-		fmt.Println("error in getting all the resources linked to a deployment: ", err)
+		fmt.Println("error in creating client set: ", err)
 		return
 	}
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	ch3 := make(chan string)
+	ch4 := make(chan string)
+	ch5 := make(chan string)
+	wg.Add(1)
+	go GetService(clientSet, *deploymentName, *deploymentNamespace, ch1)
+	wg.Add(1)
+	go GetReplicaSet(clientSet, *deploymentName, *deploymentNamespace, ch2)
+	wg.Add(1)
+	go GetConfigMap(clientSet, *deploymentName, *deploymentNamespace, ch3)
+	wg.Add(1)
+	go GetSecret(clientSet, *deploymentName, *deploymentNamespace, ch4)
+	wg.Add(1)
+	go GetVolume(clientSet, *deploymentName, *deploymentNamespace, ch5)
+	go func() {
+		wg.Wait()
+	}()
 
+	service := <-ch1
+	replicaSet := <-ch2
+	
+	for {
+		data := <-ch3
+		if data != "/done" {
+			configMapList = append(configMapList, data)
+		} else {
+			break
+		}
+	}
+
+	for {
+		data := <-ch4
+		if data != "/done" {
+			secretList = append(secretList, data)
+		} else {
+			break
+		}
+	}
+
+	for {
+		data := <-ch5
+		if data != "/done" {
+			volumeList = append(volumeList, data)
+		} else {
+			break
+		}
+	}
+
+	var configMap, secret, volume string
+	
 
 	tree := asciitree.Tree{}
 	configMapList = append(configMapList, "haha")
@@ -218,7 +272,3 @@ func main() {
 	}
 	tree.Fprint(os.Stdout, true, "")
 }
-
-
-
-
